a2fad25bad1150e7fcf1480112e3316c9081197e
yash handa <yashhanda500@gmail.com>
2020-08-02 20:08:04 +0530

    up

--- heap/code2.java
+++ heap/code2.java
@@ -9,7 +9,7 @@ public class code2{
         // PriorityQueue<Integer>pq=new PriorityQueue<>();  //min PQ  by default
         //  PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());  // max PQ
 
-        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->{
+        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->{    //comparator 
            return a-b;   //this-other    for min PQ
         // return b-a;   //other-this    for max PQ
         });
@@ -21,7 +21,36 @@ public class code2{
         while(pq.size()!=0)
         System.out.print(pq.remove()+" ");
     }
+    public static void find_k_largest(int []arr,int k){
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        int i=0;
+        int n=arr.length;
+        for(;i<k;i++)
+            pq.add(arr[i]);
+
+            for(;i<n;i++){
+                if(arr[i]>pq.peek()){
+                    pq.remove();
+                    pq.add(arr[i]);
+                }
+            }
+            return pq.peek();
+    }
+    class KthLargest {
+
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        public KthLargest(int k, int[] nums) {
+            
+        }
+        
+        public int add(int val) {
+            
+        }
+    }
     public static void main(String[]args){
-        test1();
+        // test1();
+        int[]arr={3,2,1,5,6,4};
+        int k=2;
+        System.out.println(find_k_largest(arr,k));
     }
 }
